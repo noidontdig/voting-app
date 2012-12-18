40cacc7e05d84653d542920538f183e041361db5
Alexandra Qin <alexandraqin@gmail.com>
2012-12-17 22:52:23 -0500

    can now vote on items

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import datetime
 import urllib
 import wsgiref.handlers
 
+from random import choice
+from random import sample
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.api import users
@@ -23,11 +25,11 @@ class Item(db.Model):
 def category_key(category_id):
   return db.Key.from_path('Category', int(category_id))
 
+
 class MainPage(webapp.RequestHandler):
   def get(self):
-    #category_name=self.request.get('category_name')
-    category_query = Category.all().order('-date')
-    categories = category_query.fetch(10)
+    
+    categories = Category.all()
 
     if users.get_current_user():
         url = users.create_logout_url(self.request.uri)
@@ -35,7 +37,6 @@ class MainPage(webapp.RequestHandler):
     else:
         url = users.create_login_url(self.request.uri)
         url_linktext = 'Login'
-
 
     template_values = {
         'categories': categories,
@@ -75,7 +76,8 @@ class Create(webapp.RequestHandler):
 
     new_item = self.request.get('item')
     Item(category=category,
-          content=new_item).put()
+          content=new_item,
+          votes=0).put()
 
     self.redirect('/', MainPage)
 
@@ -91,13 +93,22 @@ class Edit(webapp.RequestHandler):
         url_linktext = 'Login'
 
     category = Category.get(category_key(category_id))
-
-    template_values = {
+      
+    if (users.get_current_user() 
+      and users.get_current_user().nickname() == category.author):
+      template_values = {
         'category': category,
         'url': url,
         'url_linktext': url_linktext,
-     }
-  
+      }
+
+    else: 
+
+      error = "You do not have permission to edit this category"
+      template_values = {
+        'error': error,
+      }
+    
     path = os.path.join(os.path.dirname(__file__), 'edit.html')
     self.response.out.write(template.render(path, template_values))
 
@@ -108,21 +119,128 @@ class Edit(webapp.RequestHandler):
 
     new_item = self.request.get('item')
     Item(category=category,
-         content=new_item).put()
+         content=new_item,
+         votes=0).put()
     self.redirect("/edit/%s" % category_id)
 
 class Delete(webapp.RequestHandler):
   def get(self, thing_id):
+    
+
     cat = Category.get(category_key(thing_id))
     item = Item.get(item_key(thing_id))
     if cat:
-      cat.delete()
-      self.redirect('/', MainPage)
+      if (users.get_current_user() 
+      and users.get_current_user().nickname() == cat.author):
+        cat.delete()
+        self.redirect('/', MainPage)
+      else:
+        error = "You do not have permission to delete this category"
+        template_values = {
+          'error': error,
+        }
+        path = os.path.join(os.path.dirname(__file__), 'edit.html')
+        self.response.out.write(template.render(path, template_values))
+
     if item:
-      item_category = item.category
-      category_id = item_category.key().id()
-      item.delete()
-      self.redirect("/edit/%s" % category_id)
+      if (users.get_current_user() 
+      and users.get_current_user().nickname() == item.category.author):
+      
+        item_category = item.category
+        category_id = item_category.key().id()
+        item.delete()
+        self.redirect("/edit/%s" % category_id)
+      else:
+        error = "You do not have permission to delete this item"
+        template_values = {
+          'error': error,
+        }
+        path = os.path.join(os.path.dirname(__file__), 'edit.html')
+        self.response.out.write(template.render(path, template_values))
+
+class Vote(webapp.RequestHandler):
+  def get(self, category_id):
+    
+    category = Category.get(category_key(category_id))
+    myObjects = list(category.items)
+
+    if len(myObjects) < 2:
+      error = "The category must have at least two items to vote on"
+      template_values = {
+        'error': error
+      }
+      path = os.path.join(os.path.dirname(__file__), 'edit.html')
+      self.response.out.write(template.render(path, template_values))
+
+    else:
+      choose = sample(myObjects, 2)
+      
+      if users.get_current_user():
+          url = users.create_logout_url(self.request.uri)
+          url_linktext = 'Logout'
+      else:
+          url = users.create_login_url(self.request.uri)
+          url_linktext = 'Login'
+
+      template_values = {
+          'category': category,
+          'item1': choose[0],
+          'item2': choose[1],
+          'url': url,
+          'url_linktext': url_linktext,
+      }
+
+      path = os.path.join(os.path.dirname(__file__), 'vote.html')
+      self.response.out.write(template.render(path, template_values))
+
+  def post(self, category_id):
+
+    category = Category.get(category_key(category_id))
+    skip = self.request.get('skip')
+    if not skip:
+      voted = True
+      pair = self.request.get('pair')
+      winner_id = self.request.get('vote')
+      items = pair.split(',')
+      if winner_id == items[0]:
+        loser_id = items[1]
+      else:
+        loser_id = items[0]
+
+      winner = Item.get(item_key(winner_id))
+      loser = Item.get(item_key(loser_id))
+      
+      winner.votes += 1
+      winner.put()
+
+    else:
+      voted = False
+      winner = ""
+      loser = ""
+
+    myObjects = list(category.items)
+    choose = sample(myObjects, 2)
+
+    if users.get_current_user():
+        url = users.create_logout_url(self.request.uri)
+        url_linktext = 'Logout'
+    else:
+        url = users.create_login_url(self.request.uri)
+        url_linktext = 'Login'
+
+    template_values = {
+        'voted': voted,
+        'category': category,
+        'winner': winner,
+        'loser': loser,
+        'item1': choose[0],
+        'item2': choose[1],
+        'url': url,
+        'url_linktext': url_linktext,
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'vote.html')
+    self.response.out.write(template.render(path, template_values))
 
 
 def item_key(item_id):
@@ -148,6 +266,7 @@ class Results(webapp.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'results.html')
     self.response.out.write(template.render(path, template_values))
 
+
     
 
 
@@ -157,6 +276,7 @@ application = webapp.WSGIApplication(
                        ('/edit', Edit),
                        (r'/edit/(.*)', Edit),
                        (r'/delete/(.*)', Delete),
+                       (r'/vote/(.*)', Vote),
                        ('/results', Results)],
                        debug=True)
 
